1ff02dc308b78e8f896fe427d745adc34eb59c6a
Brooks Mershon <brooks@sketchup.com>
2017-05-27 11:42:48 -0600

    add comment to build

--- chapter-8-lambda-the-ultimate/evens-only*.rkt
+++ chapter-8-lambda-the-ultimate/evens-only*.rkt
@@ -47,6 +47,10 @@
                                          (f (cons first-nest rest-nest)
                                             (join first-odds rest-odds))))))))))
 
+; Used as the function argument for evens-only*&f.
+; Simply reports the two collected lists of the nested
+; list of evens only and the flat list of atoms containing
+; the odd entires that were removed from the nested S-expression.
 (define build
   (lambda (l1 l2)
     (cons l1 (cons l2 '()))))
